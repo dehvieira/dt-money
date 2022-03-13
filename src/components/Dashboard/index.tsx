263e075e579567159e18610a8c7df108b817ff6c
dehvieira <andrevieira_@outlook.com>
2022-03-13 20:11:49 -0300

    feat: add Dashboard

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Summary } from 'components/Summary';
+import React from 'react';
+
+import { Container } from './styles';
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Summary />
+    </Container>
+  );
+}
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+import { Container } from './styles';
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <strong>R$1200,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt="Saidas" />
+        </header>
+        <strong> -R$400,00</strong>
+      </div>
+
+      <div className="total-background">
+        <header>
+          <p>Valor Total</p>
+          <img src={totalImg} alt="Valor total" />
+        </header>
+        <strong>R$800,00</strong>
+      </div>
+    </Container>
+  );
+}
