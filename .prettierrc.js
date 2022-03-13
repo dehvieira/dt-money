8143ccfaa2634c1422e63b02e948c3a51b1f1f67
dehvieira <andrevieira_@outlook.com>
2022-03-13 17:22:24 -0300

    :tada: config aplication finished

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,9 +2,11 @@ module.exports = {
   semi: true,
   trailingComma: 'all',
   singleQuote: true,
-  printWidth: 90,
+  printWidth: 120,
   tabWidth: 2,
   endOfLine: 'auto',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
 };
 
 // {
